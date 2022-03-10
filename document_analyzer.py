b6e7a016e277a40fa373f9f643092a3e335712e0
Jordan Pastoral <pastoraljordan23@gmail.com>
2022-03-09 17:51:23 -0800

    Changing wordcount to be a list holding tuples instead of a dictionary

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,29 +1,31 @@
-def document_analyzer(textfile):
+def find_freq_words(textfile):
     # Creates a list of all the words in the document
     file = open(textfile, encoding='utf8').read()
-    allwords = file.split(" ")
+    all_words = file.split(" ")
 
-    print(allwords)
     # A list that will contain the words without repeats
     words = []
 
     # Goes through all the words to add all the words(not repeated) into the list
-    for word in allwords:
+    for word in all_words:
         if word not in words:
-            #print(word)
             words.append(word)
 
     # List to hold the word and how many times it appears in the document
-    wordcount = {}
+    wordcount = []
     for word in words:
         count = 0
-        for repeat in allwords:
+        for repeat in all_words:
             if repeat == word:
                 count += 1
-        wordcount.update({count: word})
+        wordcount.append((count, word))
 
-    worditems = wordcount.items()
-    sortedwords = sorted(worditems, reverse=True)
+    def number(item):
+        return item[0]
 
-    for key, value in sortedwords[0:5]:
+    # print(wordcount)
+    wordcount = sorted(wordcount, key=number, reverse=True)
+    # print(wordcount)
+
+    for key, value in wordcount[0:5]:
         print(f"{value}: {key}")
