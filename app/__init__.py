10ad7e17778654bba143b46f8ee7d8b012c329c9
Jordan Pastoral <pastoraljordan23@gmail.com>
2022-03-28 21:34:59 -0700

    Added files for HP question

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
 myapp_obj = Flask(__name__)
-
+myapp_obj.secret_key = 'cmpe131'
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import myapp_obj
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 
 name = "Lisa"
 city_names = ["Paris", "London", "Rome", "Tahiti"]
 
-@myapp_obj.route("/")
+@myapp_obj.route("/", methods = ['POST', 'GET'])
 def home():
-    return render_template('home.html', name = name, city_names = city_names)
+    if request.method == 'POST':
+        user = request.form["city"]
+        flash(user)
+        return redirect(url_for('home'))
+    return render_template("home.html", name = name, city_names = city_names)
