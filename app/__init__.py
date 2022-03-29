2344b629f80b0730798247f8388b27151c71b19e
Jordan Pastoral <pastoraljordan23@gmail.com>
2022-03-28 21:38:25 -0700

    Changed myapp_obj to myobj because of autograder

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
 
-myapp_obj = Flask(__name__)
-myapp_obj.secret_key = 'cmpe131'
+myobj = Flask(__name__)
+myobj.secret_key = 'cmpe131'
 from app import routes
